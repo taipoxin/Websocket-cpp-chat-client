188f4196b0cd83dd08421e78c11e3606548d20e4
Dmitry Ermakovich <ermakovichdmitriy@gmail.com>
2019-11-11 16:53:48 +0300

    updated not bad

--- MeaningOfLife.Cpp/Logic.cpp
+++ MeaningOfLife.Cpp/Logic.cpp
@@ -4,12 +4,16 @@
 #include "Poco/MD5Engine.h"
 #include "Poco/DigestStream.h"
 
+#include <websocketpp/client.hpp> 
+#include <websocketpp/config/asio_no_tls_client.hpp>
+
 #include <iostream>
 
-
+typedef websocketpp::client<websocketpp::config::asio_client> client;
 
 int MeaningOfLife::Cpp::Logic::Get() const
 {
+	client cl;
 	Poco::MD5Engine md5;
 	Poco::DigestOutputStream ds(md5);
 	ds << "abcdefghijklmnopqrstuvwxyz";
