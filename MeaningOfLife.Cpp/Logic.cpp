0f683a01f6491234fab7549ad40b82894e38f13c
Dmitry Ermakovich <ermakovichdmitriy@gmail.com>
2019-11-14 02:32:09 +0300

    add timeout

--- MeaningOfLife.Cpp/Logic.cpp
+++ MeaningOfLife.Cpp/Logic.cpp
@@ -4,10 +4,26 @@
 #include "Logic.h"
 #include "ws_client_orig.h"
 #include "CastCoreUtils.h"
-
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
+int connectWS(websocket_endpoint &endpoint, string s, double timeoutS) {
+	int id = -10;
+	clock_t this_time = clock();
+	clock_t last_time = this_time;
+	id = endpoint.connect(s);
+	connection_metadata::ptr metadata = endpoint.get_metadata(id);
+	
+	while ((metadata.get()->get_status() != "Open") || (timeoutS * CLOCKS_PER_SEC > (double)(this_time - last_time))) {
+		this_time = clock();
+	}
+	if (timeoutS * CLOCKS_PER_SEC < (double)(this_time - last_time)) {
+		cout << "Connection Timeout" << endl;
+	}
+	return id;
+}
 
 wstring MeaningOfLife::Cpp::Logic::Get(string text) const
 {
@@ -18,14 +34,13 @@ wstring MeaningOfLife::Cpp::Logic::Get(string text) const
 	cout << "a" << endl;
 
 	int id = -10;
-	id = endpoint.connect(s);
-	for (int j = 0; j < 10000000; j++) {
-		cout << "";
-	}
+	id = connectWS(endpoint, s, 0.5);
+
 	connection_metadata::ptr metadata = endpoint.get_metadata(id);
 	if (metadata) {
 		cout << *metadata << endl;
 	}
+	
 
 	string two = "Русские вперед " + metadata.get()->get_status();
 	wstring wide = string_to_wstring(two);
