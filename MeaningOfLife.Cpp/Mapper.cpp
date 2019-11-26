7d4062b0a22c7165fee4f785c77a5653a6752975
Dmitry Ermakovich <ermakovichdmitriy@gmail.com>
2019-11-26 14:08:19 +0300

    Wallet example add

--- MeaningOfLife.Cpp/Mapper.cpp
+++ MeaningOfLife.Cpp/Mapper.cpp
@@ -86,3 +86,215 @@ int isExistsExample() {
 
 	return 0;
 }
+
+
+int eraseElExample() {
+
+	// Map of string & int i.e. words as key & there
+	// occurrence count as values
+	std::map<std::string, int> wordMap = { { "is", 6 }, { "the", 5 },
+			{ "hat", 9 }, { "at", 6 }, { "of", 2 }, { "hello", 1 } };
+
+
+	std::cout << "Map Entries Before Deletion" << std::endl;
+	// Print the map elements
+	for (auto elem : wordMap)
+		std::cout << elem.first << " :: " << elem.second << std::endl;
+
+	// Create an iterator pointing to begin of map
+	std::map<std::string, int>::iterator it1 = wordMap.begin();
+
+	// Create an iterator pointing to begin of map
+	std::map<std::string, int>::iterator it2 = wordMap.begin();
+	// Increment Iterator
+	it2++;
+	// Increment Iterator
+	it2++;
+	// Itr2 is now pointing to 3rd element
+
+
+	// Check if iterator is valid.
+	if (it1 != wordMap.end() && it2 != wordMap.end())
+	{
+		// Remove the element pointed by iterator
+		wordMap.erase(it1, it2);
+		std::cout << "Elements Removed" << std::endl;
+	}
+	else
+		std::cout << "Key Not Found" << std::endl;
+
+
+	std::cout << "Map Entries After Deletion" << std::endl;
+	// Print the map elements
+	for (auto elem : wordMap)
+		std::cout << elem.first << " :: " << elem.second << std::endl;
+
+	return 0;
+}
+
+
+#include <unordered_map>
+#include <string>
+
+int unorderedMap() {
+	// Initialize an unordered_map through initializer_list
+	std::unordered_map<std::string, int> wordMap({
+		{ "First", 1 }, { "Second",	2 }, { "Third", 3 }
+		});
+
+	// Declare an iterator to unordered_map
+	std::unordered_map<std::string, int>::iterator it;
+
+	// Find if an element with key "First" exists or not.
+	// find() returns an iterator
+
+	it = wordMap.find("First");
+
+	// Check if iterator points to end of map
+	if (it != wordMap.end())
+	{
+		std::cout << "Element Found - ";
+		std::cout << it->first << "::" << it->second << std::endl;
+	}
+	else
+	{
+		std::cout << "Element Not Found" << std::endl;
+	}
+
+	return 0;
+}
+
+
+#include <iostream>
+#include <set>
+#include <string>
+#include <algorithm>
+
+
+/*
+ * Access nth element from a set.
+ * It returns a pair of Element and bool.
+ * bool represents if nth element exists in set or not.
+ * if bool is true them First element of pair T contains the element value
+ */
+	template <typename T>
+std::pair<T, bool> getNthElement(std::set<T> & searchSet, int n)
+{
+	std::pair<T, bool> result;
+	if (searchSet.size() > n)
+	{
+		result.first = *(std::next(searchSet.begin(), n));
+		result.second = true;
+	}
+	else
+		result.second = false;
+
+	return result;
+}
+
+int setByIndex()
+{
+	std::set<std::string> setOfStr =
+	{ "bb", "ee", "dd", "aa", "ll" };
+
+	std::cout << "***** Set Contents *****" << std::endl;
+	for (std::string elem : setOfStr)
+		std::cout << elem << std::endl;
+
+	std::cout << "***** Accessing Elements by Index ***" << std::endl;
+
+	// Access 3rd element
+	std::pair<std::string, bool> result = getNthElement(setOfStr, 3);
+
+	if (result.second)
+		std::cout << "3rd Element in set = " << result.first << std::endl;
+	else
+		std::cout << "3rd Element in set not found" << std::endl;
+
+
+	// Access 7th element
+	result = getNthElement(setOfStr, 7);
+
+	if (result.second)
+		std::cout << "7th Element in set = " << result.first << std::endl;
+	else
+		std::cout << "7th Element in set not found" << std::endl;
+
+}
+
+#include<iostream>
+#include<thread>
+#include<vector>
+#include<mutex>
+class Wallet
+{
+	int mMoney;
+	std::mutex mutex;
+public:
+	Wallet() :mMoney(0) {}
+	int getMoney() { return mMoney; }
+	void addMoney(int money)
+	{
+		mutex.lock();
+		for (int i = 0; i < money; ++i)
+		{
+			mMoney++;
+		}
+		mutex.unlock();
+	}
+};
+
+
+int testMultithreadedWallet()
+{
+	Wallet walletObject;
+	std::vector<std::thread> threads;
+	for (int i = 0; i < 5; ++i) {
+		threads.push_back(std::thread(&Wallet::addMoney, &walletObject, 1000));
+	}
+	for (int i = 0; i < threads.size(); i++)
+	{
+		threads.at(i).join();
+	}
+	return walletObject.getMoney();
+}
+int WalletThread()
+{
+	int val = 0;
+	for (int k = 0; k < 1000; k++)
+	{
+		if ((val = testMultithreadedWallet()) != 5000)
+		{
+			std::cout << "Error at count = " << k << "  Money in Wallet = " << val << std::endl;
+			//break;
+		}
+	}
+	return 0;
+}
+
+
+int testMultithreadedWallet()
+{
+	Wallet walletObject;
+	std::vector<std::thread> threads;
+	for (int i = 0; i < 5; ++i) {
+		threads.push_back(std::thread(&Wallet::addMoney, &walletObject, 1000));
+	}
+	for (int i = 0; i < threads.size(); i++)
+	{
+		threads.at(i).join();
+	}
+	return walletObject.getMoney();
+}
+int multiWallet()
+{
+	int val = 0;
+	for (int k = 0; k < 1000; k++)
+	{
+		if ((val = testMultithreadedWallet()) != 5000)
+		{
+			std::cout << "Error at count = " << k << " Money in Wallet = " << val << std::endl;
+		}
+	}
+	return 0;
+}
